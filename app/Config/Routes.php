adb8befb1d6f41b843c697b8c9f63f5dc596c800
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2023-09-22 09:22:17 +0700

    database-model

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,4 +6,8 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
-$routes->get('/profile/(:any)/(:any)/(:any)/', [Home::class, 'profile']);
+$routes->get('user/profile/(:any)/(:any)/(:any)/(:any)/(:any)/', 'UserController::class::profile');
+
+#form
+$routes->get('/user/create', 'UserController::create');
+$routes->post('/user/store', 'UserController::store');
