a44598fecdd5dad6d55c4de8e76b6890b8822537
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2023-10-14 08:10:04 +0700

    update-delete

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -15,6 +15,11 @@ $routes->get('/user', 'UserController::index');
 $routes->get('/user/create', 'UserController::create');
 $routes->post('/user/store', 'UserController::store');
 $routes->get('/user/create', 'UserController::create');
+
+$routes->get('/user/(:any)/edit', 'UserController::edit/$1');
+$routes->put('/user/(:any)', 'UserController::update/$1');
+$routes->delete('/user/(:any)', 'UserController::destroy/$1');
+
 $routes->get('/user/profile/(:any)', 'UserController::show/$1');
 
 // $routes->get('/user/profile', [UserController::class, 'profile']);
